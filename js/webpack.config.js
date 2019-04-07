450b711720eab7f72b5321baacec67507f820f2a
yes7 <hyoka4654@gmail.com>
2019-04-08 01:36:44 +0800

    npm init

---
+++ js/webpack.config.js
@@ -0,0 +1,9 @@
+let path = require('path');
+module.exports = {
+    entry : './js/script.js',
+    output:{
+        path:path.join(__dirname,'dist'),
+        filename:'bundle.js',
+        publicPath:'./'
+    }
+}
